edddb602bbac572fa4fbb20eb2d9f466889ea76a
Ameika <ameika2900@gmail.com>
2017-11-15 00:35:21 +0900

    조서현
    커밋을 생활화

--- auto-pyinstaller/auto_pyinstaller.py
+++ auto-pyinstaller/auto_pyinstaller.py
@@ -1,26 +1,34 @@
-import os
+import os,time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 def runPyInstaller(file):
+    file = file.split('\\')[-1]
+    print(file + ' => exe파일로 빌드중...')
     return os.popen('pyinstaller ' + file + ' -F')
 
-class fileWatcher(PatternMatchingEventHandler):  
-    def on_modified(self,event):
-        print('파일내 변경  - ' + event.src_path)
+class fileWatcher(PatternMatchingEventHandler):    
+    def on_modified(self,event):        
+        time.sleep(0.1)
         runPyInstaller(event.src_path)
+        super()
 
 def watchPy():
     observer = Observer()
     observer.schedule(fileWatcher(patterns=["*.py","*.pyw"]), os.getcwd(), recursive=True)
-    observer.start()   
-
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        exit()
+       
 def installerStarter():
     lastFile = False
     cnt = 0
     for file in os.listdir():
         if file.endswith('.py'):
-            print(file + ' => exe파일로 빌드중...')
             lastFile = runPyInstaller(file)
             cnt += 1
     return lastFile,cnt
@@ -35,5 +43,6 @@ if __name__ == '__main__':
         endMsg = '대상 파일이 없습니다.'
     print(endMsg)
     print(os.getcwd() + ' 경로의 *.py , *.pyw 파일 감시를 시작합니다.')
-    watchPy() 
+    watchPy()
+    
            
